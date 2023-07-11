3de9688f94de6811e6d48bb56da9517c3db26d82
Kaitlyn <kmbrandt08@hotmail.com>
2023-07-10 20:25:11 -0500

    forgot footer :(

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link'
 import React from 'react'
+
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div><Link href="https://kbrandt-portfolio.vercel.app/">Back to Portfolio</Link></div>
   )
 }
 
