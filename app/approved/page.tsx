36b29a1bc581080ecabe0ddb87bb2e10cdb35cc6
Kaitlyn <kmbrandt08@hotmail.com>
2023-10-18 17:57:49 -0500

    Added cookie expiration date due to the deployed app cookie error

--- app/approved/page.tsx
+++ app/approved/page.tsx
@@ -1,11 +1,6 @@
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 export default function Page() {
-  const cookieStore = cookies()
-  const session = cookieStore.get('session')?.value || undefined
-if(session){
     redirect('/')
-}
 
   return (
 <h1>Logging in...</h1>
